d708a4ce05ae7f93955190448f07e68798738170
Thomas Lacroix <toto.rigolo@free.fr>
2020-02-23 18:03:35 +0100

    Remove wee_alloc (incompatible with stdweb)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 mod app;
 
-// Use `wee_alloc` as the global allocator.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 fn main() {
     yew::start_app::<app::Model>();
 }
